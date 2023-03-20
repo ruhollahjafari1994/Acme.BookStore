e91bd826bcbc8a905d2f788fbd267a782b234de0
ruhollah_jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2023-03-20 18:33:02 +0330

    Compelete-Part6 Web Application Development Tutorial - Part 6: Authors: Domain Layer

---
+++ src/Acme.BookStore.Domain/Authors/AuthorAlreadyExistsException.cs
@@ -0,0 +1,16 @@
+﻿using Volo.Abp;
+
+namespace Acme.BookStore.Authors;
+
+public partial class AuthorManager
+{
+    public class AuthorAlreadyExistsException : BusinessException
+{
+    public AuthorAlreadyExistsException(string name)
+        : base(BookStoreDomainErrorCodes.AuthorAlreadyExists)
+    {
+        WithData("name", name);
+    }
+}
+
+}
